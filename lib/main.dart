85a2f06c3dbf4cf11cbdc0fe74c4866d3f7a938f
lucky okirie <okirielucky1304@gmail.com>
2023-08-04 17:15:11 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-//import 'package:burger/pages/food_detail_page/food_detail.dart';
-import 'package:burger/controllers/porpular_product_controller.dart';
-import 'package:burger/dependencies/dependencies.dart';
-import 'package:burger/pages/food_detail_page/recommended_food_detail.dart';
-//import 'package:burger/pages/home_page/main_food_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await init();
+
   runApp(const MyApp());
 }
 
@@ -18,14 +13,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Get.find<PorpularProductControler>().getPorpularProductList();
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RecommendedFoodDetail(),
+      home: const Scaffold(),
     );
   }
 }
--- lib/utils/app_oonstants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  static const String APP_NAME = "KCBurger";
-  static const int APP_VERSION = 1;
-
-  
-  static const String BASE_URL = "http://luckyportfolio.pythonanywhere.com";
-  
-}
